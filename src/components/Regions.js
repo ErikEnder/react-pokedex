3bfbcb93f620efbb1f1816b7d40e8a53a90f92d3
Erik Ender <45837117+ErikEnder@users.noreply.github.com>
2023-05-16 14:08:04 -0500

    refactor API call and added Recoil

---
+++ src/components/Regions.js
@@ -0,0 +1,41 @@
+import { React, useEffect } from 'react'
+import { useRecoilState } from 'recoil'
+import { regionAtom } from '../state/region-state'
+import axios from 'axios'
+
+export default function DisplayRegions() {
+  const [region, setRegion] = useRecoilState(regionAtom)
+
+  useEffect(() => {
+    const fetchRegion = async () => {
+      try {
+        const response = await axios.get('https://pokeapi.co/api/v2/generation/1/')
+        console.log(response.data)
+        setRegion(response.data)
+      }catch(error) {
+        console.log(error)
+        return []
+      }
+    }
+    fetchRegion()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  console.log('Region: ', region)
+
+  function SortPokemon() {
+    region.pokemon_species.map((species, index) => {
+      return (
+        <>
+          <p>{species.name}</p>
+        </>
+      )
+    })
+  }
+  
+  return (
+    <>
+      {SortPokemon()}
+    </>
+  )
+}
